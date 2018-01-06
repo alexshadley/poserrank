ed427d4c24f4b7935eed689f325360effa9d9b56
Alex Shadley <shadleyalex@gmail.com>
2018-01-06 09:02:10 -0600

    removes local copy of Bulma

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html.j2')
+
+# this line makes the app run; no idea how
+if __name__ == '__main__':
+	app.run()
