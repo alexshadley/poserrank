3d65b7a9518ef739a4f149ebbf77dba73b0a24b4
Alex Shadley <shadleyalex@gmail.com>
2018-01-08 13:33:49 -0600

    adds a /top page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html.j2')
 
+@app.route('/top/')
+def top():
+	return render_template('top.html.j2')
+
 # this line makes the app run; no idea how
 if __name__ == '__main__':
 	app.run()
