f9297e725082bc6ee3ba4980a0ebc3aa9ca661b9
Alex Shadley <shadleyalex@gmail.com>
2018-01-10 22:50:07 -0600

    renames functions for consistency with python style

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ from flask import render_template
 app = Flask(__name__)
 
 # returns a connection to the database
-def getDB():
+def get_db():
 	db = sqlite3.connect('db/development.db')
 	db.row_factory = sqlite3.Row # this line establishes that we want row objects back from queries, which are useful because they act as dictionaries
 	return db
 
 # gets all posers
-def getPosers():
-	db = getDB()
+def get_posers():
+	db = get_db()
 	result = db.execute('SELECT * FROM posers ORDER BY score DESC;')
 	return result
 
 # gets the poser with queryName
-def getPoserByName(queryName):
-	db = getDB()
+def get_poser_by_name(queryName):
+	db = get_db()
 	name = (queryName,)
-	result = db.execute('SELECT * FROM posers WHERE name=?', name)
+	result = db.execute('SELECT * FROM posers WHERE name=?;', name)
 	return result
 
 @app.route('/')
@@ -28,7 +28,7 @@ def home():
 
 @app.route('/top/')
 def top():
-	posers = getPosers() # connect to the database and retrieve all posers
+	posers = get_posers() # connect to the database and retrieve all posers
 	return render_template('top.html.j2', posers=posers) # render the 'top' template, with posers as a local variable passed into the template
 
 # this line makes the app run; no idea how
