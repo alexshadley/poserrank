70fb579a4cada11d573e8e56e03903c6d02d0db0
Alex Shadley <shadleyalex@gmail.com>
2018-01-22 13:32:36 -0600

    refactors project into a package; more refactoring to come

--- poserrank/__init__.py
+++ poserrank/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 # returns a connection to the database
 def get_db():
-	db = sqlite3.connect('db/development.db')
+	db = sqlite3.connect('poserrank/db/development.db')
 	db.row_factory = sqlite3.Row # this line establishes that we want row objects back from queries, which are useful because they act as dictionaries
 	return db
 
--- poserrank/db/generate_db.py
+++ poserrank/db/generate_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def generate_db():
-	db = sqlite3.connect('db/development.db')
+	db = sqlite3.connect('development.db')
 	db.execute('CREATE TABLE posers (name text, score integer);')
 
 if __name__ == '__main__':
