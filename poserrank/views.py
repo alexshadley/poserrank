923ddecb88dfd0ee7a36807728237bb70503eb1d
Alex Shadley <shadleyalex@gmail.com>
2018-01-22 22:45:55 -0600

    adds the login endpoint and login form

--- poserrank/views.py
+++ poserrank/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from poserrank import app
 from poserrank import database as db
 
@@ -10,3 +10,11 @@ def index():
 def top():
 	posers = db.get_posers() # connect to the database and retrieve all posers
 	return render_template('top.html.j2', posers=posers) # render the 'top' template, with posers as a local variable passed into the template
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html.j2')
+
+    if request.method == 'POST':
+        return "placeholder"
