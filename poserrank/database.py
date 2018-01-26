2595427731c9bb3466414dc713d5e0d91867c8da
Alex Shadley <shadleyalex@gmail.com>
2018-01-25 20:31:18 -0600

    removes unused database logic

--- poserrank/database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# returns a connection to the database
-def get_db():
-	db = sqlite3.connect('poserrank/db/development.db')
-	db.row_factory = sqlite3.Row # this line establishes that we want row objects back from queries, which are useful because they act as dictionaries
-	return db
