db6ebd471ce12bf93df04ea4738388a25051b722
Alex Shadley <shadleyalex@gmail.com>
2018-01-25 22:09:27 -0600

    adds a simple new user page and underlying infrastructure

--- poserrank/views.py
+++ poserrank/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, request, session
-from poserrank import app
+from poserrank import app, db
 from poserrank.models import User
 
 @app.route('/')
@@ -11,16 +11,19 @@ def top():
 	users = User.query.all() # connect to the database and retrieve all posers
 	return render_template('top.html.j2', users=users) # render the 'top' template, with posers as a local variable passed into the template
 
+# web browsers initially request this page with GET; after the user has filled
+# out the form, the 'sign in' button makes a POST request to the same endpoint,
+# this time with the login credentials stored in the request
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-	if request.method == 'GET':
+	if request.method == 'GET': # just serve the login page if it's a GET request
 		return render_template('login.html.j2')
 
-	if request.method == 'POST':
-		query = User.query.filter(User.username == request.form['username'])
-		if query.count() > 0:
+	if request.method == 'POST': # authenticate the user if it's a POST request
+		query = User.query.filter(User.username == request.form['username']) # query the database for users with the entered username
+		if query.count() > 0: # check if any results came up
 			user = query.first()
-			if user.password == request.form['password']:
+			if user.password == request.form['password']: # if the passwords match, log the user in
 				session['username'] = user.username
 				session['authenticated'] = True
 				return redirect(url_for('index'))
@@ -30,9 +33,24 @@ def login():
 		else:
 			return request.form['username'] + ' does not exist'
 
+# simple endpoint to log the current user out
 @app.route('/logout/')
 def logout():
 	if session['authenticated']:
 		session.pop('username', None)
 		session['authenticated'] = False
 	return redirect(url_for('index'))
+
+@app.route('/newuser/', methods=['GET', 'POST'])
+def newuser():
+	if request.method == 'GET':
+		return render_template('newuser.html.j2')
+
+	elif request.method == 'POST':
+		newUser = User(username=request.form['username'],
+					full_name=request.form['full_name'],
+					email=request.form['email'],
+					password=request.form['password'])
+		db.session.add(newUser)
+		db.session.commit()
+		return redirect(url_for('index'))
